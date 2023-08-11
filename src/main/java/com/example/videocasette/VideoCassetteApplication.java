016597e6b6c4159c6ab91c3519b1b8581f05c866
Marta Dylewska <mdylewska@bravurasolutions.com>
2023-08-11 12:51:34 +0200

    Naming error corrected

--- src/main/java/com/example/videocasette/VideoCassetteApplication.java
+++ src/main/java/com/example/videocasette/VideoCassetteApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VideoCasetteApplication {
+public class VideoCassetteApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(VideoCasetteApplication.class, args);
+        SpringApplication.run(VideoCassetteApplication.class, args);
     }
 
 }
