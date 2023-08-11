2bd2bd3f8a2d48da017bc9c968466e19efc8aef8
Marta Dylewska <mdylewska@bravurasolutions.com>
2023-08-11 10:47:40 +0200

    Initial commit with core files

---
+++ src/main/java/com/example/videocasette/VideoCasetteApplication.java
@@ -0,0 +1,13 @@
+package com.example.videocasette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoCasetteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoCasetteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/videocasette/VideoCasetteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.videocasette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoCasetteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
